340d9c81e6191bfd68c788072801dea8ed75d8da
Git-peanutsuu <ntsports18@gmail.com>
2023-01-25 16:23:34 +0900

    to app.from_pyfile

--- app/__init__.py
+++ app/__init__.py
@@ -27,12 +27,12 @@ def create_app(mode='DEV'):
     app= Flask(__name__, template_folder="templates", instance_path="/app/instance/")
     if mode == 'DEV':
         app.config.from_object(DefaultConfig)
-        app.config.from_envvar('FLASK_CONFIG_ENVVAR')
+        app.config.from_pyfile('config.cfg')
         # app.config.from_pyfile('config.cfg') 
         # Vscode can not recognize instance folder appropreately because of Pipenv set in the working directory
     else:
         app.config.from_object(ProductionConfig)
-        app.config.from_envvar('FLASK_CONFIG_ENVVAR')
+        app.config.from_envvar('config.cfg')
         # app.config.from_pyfile('config.cfg')
 
     #https://msiz07-flask-docs-ja.readthedocs.io/ja/latest/config.html
